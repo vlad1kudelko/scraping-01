f841186d8ce0fa7ee589e1dc6b6e412bdfa78a3b
name <you@example.com>
2023-08-16 20:24:51 +0800

    -

--- src/main.py
+++ src/main.py
@@ -1,30 +1,21 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import undetected_chromedriver
+import undetected_chromedriver as webdriver
 
 #--------------------------------------------------------------------
 def main():
     #  url = 'https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html'
     url = 'https://www.lieferando.at/speisekarte/vapiano-wien-herrengasse'
-    #  url = 'https://www.lieferando.de/'
     #  url = 'https://nowsecure.nl/'
 
-    driver = undetected_chromedriver.Chrome()
-    driver.get(url)
+    driver = webdriver.Chrome()
 
-    ############### Cloudflare bypass ###############
-    # adspower.com/ru/blog/bypass-cloudflare-detection-with-selenium
-    handle = driver.current_window_handle
-    driver.service.stop()
-    time.sleep(6)
-    driver = undetected_chromedriver.Chrome()
-    driver.switch_to.window(handle)
-    ############### Cloudflare bypass ###############
-    print('good')
+    driver.get(url)
+    driver.reconnect(timeout=6)
 
     time.sleep(3)
     driver.find_element(By.CSS_SELECTOR, '*[data-qa="restaurant-header-rating-action"]').click()
+    driver.find_element(By.CSS_SELECTOR, '*[data-qa=restaurant-info-modal-reviews] div[data-qa=heading]').text
 
     time.sleep(9999)
 #--------------------------------------------------------------------
