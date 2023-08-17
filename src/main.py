7b31ba394fba0f9bd95f4d4a9e8681f365d9ac37
name <you@example.com>
2023-08-17 09:56:42 +0800

    -

--- src/main.py
+++ src/main.py
@@ -4,7 +4,47 @@ import time
 import undetected_chromedriver as webdriver
 
 #--------------------------------------------------------------------
-def parse_page(inp_url):
+def parse_page_de(inp_url):
+    ret = {}
+
+    driver.get(inp_url)
+    driver.reconnect(timeout=10)
+    driver.implicitly_wait(5)
+
+    driver.find_element(By.CSS_SELECTOR, '[data-qa=restaurant-header-rating-action]').click()
+    ret['rating'] = driver.find_element(By.CSS_SELECTOR, '[data-qa=restaurant-info-modal-reviews] div[data-qa=heading]').text
+    ret['count'] = driver.find_element(By.CSS_SELECTOR, '[data-qa=restaurant-info-modal-reviews-rating-element] ~ * > *:nth-child(1)').text
+    ret['count'] = re.findall(r'\d+', ret['count'].split('\n')[1])[0]
+    ret['list'] = []
+
+    gameover= {'old_count': 0, 'count_iter': 0}
+    while len(ret['list']) != ret['count']:
+        # gameover
+        if gameover['old_count'] == len(ret['list']):
+            gameover['count_iter'] += 1
+            print('strike', gameover)
+        else:
+            gameover['count_iter'] = 0
+        if gameover['count_iter'] == 5:
+            break
+        gameover['old_count'] = len(ret['list'])
+        # gameover
+        driver.execute_script('document.querySelector("[data-qa=modal-scroll-content]").scrollTo(0, 99999999)')
+        list_cards = driver.find_elements(By.CSS_SELECTOR, '[data-qa=review-card-component-element]')
+        for item_cards in list_cards:
+            current_id = item_cards.find_element(By.CSS_SELECTOR, '[id^=label]').get_attribute('id').split('-')[1]
+            if current_id not in [ i['id'] for i in ret['list'] ]:
+                ret['list'].append({
+                    'id': current_id,
+                    'name': item_cards.find_element(By.CSS_SELECTOR, '[id^=label] > *:nth-child(1) > *:nth-child(1)').text,
+                    'date': item_cards.find_element(By.CSS_SELECTOR, '[id^=label] > *:nth-child(1) > *:nth-child(2)').text,
+                })
+                print('len:', len(ret['list']) )
+        time.sleep(1)
+
+    return ret
+#--------------------------------------------------------------------
+def parse_page_en(inp_url):
     ret = {}
 
     driver.get(inp_url)
@@ -49,7 +89,21 @@ def main():
     url = 'https://www.lieferando.at/speisekarte/vapiano-wien-herrengasse'
     #  url = 'https://nowsecure.nl/'
 
-    print(parse_page(url))
+    list_url = [
+        {'url': 'https://www.lieferando.at/speisekarte/vapiano-wien-herrengasse',           'group': 'de'},
+        {'url': 'https://www.lieferando.de/speisekarte/peter-pane-hamburg-goldbekplatz',    'group': 'de'},
+        {'url': 'https://www.thuisbezorgd.nl/de/speisekarte/vapiano-rembrandtplein',        'group': 'de'},
+        {'url': 'https://www.pyszne.pl/menu/vapiano-galeria-mokotow',                       'group': 'de'},
+        {'url': 'https://www.just-eat.ch/en/menu/vapiano-zuerich-raemistrasse',             'group': 'de'},
+
+        {'url': 'https://www.just-eat.co.uk/restaurants-vapiano-manchester',                'group': 'en'},
+        {'url': 'https://www.just-eat.es/restaurants-vapiano-barcelona',                    'group': 'en'},
+        {'url': 'https://www.menulog.com.au/restaurants-vapiano-king-st-sydney',            'group': 'en'},
+    ]
+
+    for item_url in list_url:
+        if item_url['group'] == 'de':
+            ret = parse_page_de(item_url['url'])
 #--------------------------------------------------------------------
 if __name__ == '__main__':
     driver = webdriver.Chrome()
